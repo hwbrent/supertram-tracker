78909401ead340c2dd67bb44cf158132686e2dc6
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-08-10 23:01:02 +0100

    Try parsing document
    Not working cos it's sussed im a bot and isn't giving me the html i'm trying to get

--- packages/data-sources/fetch/routes.ts
+++ packages/data-sources/fetch/routes.ts
@@ -2,19 +2,37 @@ import axios from 'axios';
 import { JSDOM } from 'jsdom';
 
 enum URLs {
+    PREFIX = 'https://www.travelsouthyorkshire.com/en-GB/supertram',
     /** The page that says in plain english in the HTML what the routes are */
-    ROUTES_HOMEPAGE = 'https://www.travelsouthyorkshire.com/en-GB/supertram/supertram-network-and-routes'
+    ROUTES_HOMEPAGE = URLs.PREFIX + '/supertram-network-and-routes'
 };
+
+enum Headers {
+    USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:141.0) Gecko/20100101 Firefox/141.0'
+}
 
 async function main(): Promise<void> {
     // get the homepage
-    const response = await axios.get(URLs.ROUTES_HOMEPAGE);
+    const response = await axios.get(URLs.ROUTES_HOMEPAGE, {
+        headers: {
+            'User-Agent': Headers.USER_AGENT
+        }
+    });
 
     // parse it as html
     const htmlString = response.data;
     const dom = new JSDOM(htmlString);
     const document = dom.window.document;
-    console.log(document.body.outerHTML);
+
+    // this page has the info, but the HTML is not structured in a way that makes it easy
+    // to scrape. it doesn't have classnames that delineate the different parts of the
+    // page for example. so we just have to do stuff like notice patterns in hrefs and
+    // search for those and then find elements relative to those to get the
+    // human-readable text
+
+    // find the anchor tags which link to a route's specific page
+    const anchors = document.querySelectorAll('a[href*="/supertram/"][href$="-route"]');
+    console.log(anchors.length);
 }
 
 main();
