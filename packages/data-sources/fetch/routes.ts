265a79e166202b1572bf41a7760a42e503a2da04
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-08-10 22:28:25 +0100

    Fetch homepage html

--- packages/data-sources/fetch/routes.ts
+++ packages/data-sources/fetch/routes.ts
@@ -1,6 +1,14 @@
+import axios from 'axios';
+
 enum URLs {
     /** The page that says in plain english in the HTML what the routes are */
     ROUTES_HOMEPAGE = 'https://www.travelsouthyorkshire.com/en-GB/supertram/supertram-network-and-routes'
 };
 
-console.log(URLs.ROUTES_HOMEPAGE);
+async function main(): Promise<void> {
+    // get the homepage
+    const response = await axios.get(URLs.ROUTES_HOMEPAGE);
+    console.log(response);
+}
+
+main();
