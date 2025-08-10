04cf2a8d39888ecced0d6375375c1d69745a40fc
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-08-10 22:23:20 +0100

    Add routes homepage url

--- packages/data-sources/fetch/routes.ts
+++ packages/data-sources/fetch/routes.ts
@@ -1,1 +1,6 @@
-console.log('hello world');
+enum URLs {
+    /** The page that says in plain english in the HTML what the routes are */
+    ROUTES_HOMEPAGE = 'https://www.travelsouthyorkshire.com/en-GB/supertram/supertram-network-and-routes'
+};
+
+console.log(URLs.ROUTES_HOMEPAGE);
