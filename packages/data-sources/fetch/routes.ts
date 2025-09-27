20889a2491e0bb511af553b3ea66efd218eb9df3
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-09-27 19:23:57 +0100

    Remove "Obj"

--- packages/data-sources/fetch/routes.ts
+++ packages/data-sources/fetch/routes.ts
@@ -49,8 +49,8 @@ export default async function fetchRoutes(): Promise<Route[]> {
         // create a full URL
         const url = new URL(href, URLs.BASE);
 
-        const routeObj: Route = { href, url, name, description };
-        return routeObj;
+        const route: Route = { href, url, name, description };
+        return route;
     });
 
     return results;
