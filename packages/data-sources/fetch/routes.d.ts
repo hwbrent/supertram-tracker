5e807547f783d148155ba0b29e623bad014604cc
hwbrent <68196383+hwbrent@users.noreply.github.com>
2025-09-27 13:54:21 +0100

    Explain type and its properties

--- packages/data-sources/fetch/routes.d.ts
+++ packages/data-sources/fetch/routes.d.ts
@@ -1,6 +1,21 @@
+/**
+ * @summary An object containing data on an individual route within the tram network
+ */
 type Route = {
+    /**
+     * @summary The overall name of the route
+     * @example "PURP"
+     */
     name: string;
+    /**
+     * @summary A brief description of the route
+     * @example "Herdings Park - Cathedral"
+     */
     description: string;
+    /**
+     * @summary The full URL that leads to more details about the route (timetable etc)
+     * @example "https://bustimes.org/services/purp-park-hill-sheffield-herdings"
+     */
     href: string;
 }
 
